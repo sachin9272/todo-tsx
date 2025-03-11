0c8e2e8e1eda7e92d5f6edb758ffd2fda9d0abe3
Sachin Singh <sachinsingh9272@gmail.com>
2025-03-11 23:23:47 +0530

    Implemented ToddoServices

---
+++ src/TodoService.ts
@@ -0,0 +1,37 @@
+import TodoTypes from "./todo";
+
+const LOCAL_STORAGE_KEY = "todos";
+
+const TodoService = {
+
+    //Get Todos
+    getTodos: (): TodoTypes[] => {
+        return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "[]");
+    },
+
+    //Adding Todos
+    addTodos: (text:string): TodoTypes => {
+        const todos = TodoService.getTodos();
+        const newTodo: TodoTypes = {id: todos.length + 1, text, completed: false};
+        const updateTodos = [...todos, newTodo];
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updateTodos));
+        return newTodo;
+    },
+
+    //Updating the Todos
+    updateTodo: (todo:TodoTypes): TodoTypes => {
+        const todos = TodoService.getTodos();
+        const updateTodos = todos.map((t) => t.id === todo.id ? todo : t);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updateTodos));
+        return todo;
+    },
+
+    //Deleting the Todos
+    deleteTodo: (id:number): void => {
+        const todos = TodoService.getTodos();
+        const updateTodos = todos.filter((todo) => todo.id !== id);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updateTodos));
+    }
+}
+
+export default TodoService;
