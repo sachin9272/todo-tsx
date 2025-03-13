48ed7e8fb8f9cd167dc54fc82ae64287bd869ade
Sachin Singh <sachinsingh9272@gmail.com>
2025-03-13 23:30:26 +0530

    Created basic UI structure

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,47 +1,73 @@
-import React,{useState} from 'react'
-import TodoTypes from '../todo'
-import TodoService from '../TodoService'
+import { useState } from "react";
+import TodoTypes from "../todo";
+import TodoService from "../TodoService";
 const TodoList = () => {
-    const [todos, setTodos] = useState<TodoTypes[]>(TodoService.getTodos());
-    const [editedTodoId, setEditedTodoId] = useState<number | null>(null);
-    const [editedTodoText, setEditedTodoText] = useState<string>("");
+  const [todos, setTodos] = useState<TodoTypes[]>(TodoService.getTodos());
+  const [editedTodoId, setEditedTodoId] = useState<number | null>(null);
+  const [editedTodoText, setEditedTodoText] = useState<string>("");
 
-    //Function for handling edit Actions
+  //Function for handling edit Actions
 
-    const handleEditStart = (id:number, text:string) => {
-        setEditedTodoId(id);
-        setEditedTodoText(text);
+  const handleEditStart = (id: number, text: string) => {
+    setEditedTodoId(id);
+    setEditedTodoText(text);
+  };
+
+  const handleEditCancel = () => {
+    setEditedTodoId(null);
+    setEditedTodoText("");
+  };
+
+  const handleEditSave = (id: number) => {
+    if (editedTodoText.trim() !== "") {
+      const updateTodo = TodoService.updateTodo({
+        id,
+        text: editedTodoText,
+        completed: false,
+      });
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === id ? updateTodo : todo))
+      );
+      setEditedTodoId(null);
+      setEditedTodoText("");
     }
+  };
 
-    const handleEditCancel = () => {
-        setEditedTodoId(null);
-        setEditedTodoText("");
-    }
-
-    const handleEditSave =(id:number) => {
-        if(editedTodoText.trim() !== ""){
-            const updateTodo = TodoService.updateTodo({
-                id,
-                text: editedTodoText,
-                completed: false
-            });
-            setTodos((prevTodos) => prevTodos.map((todo) => (todo.id === id ? updateTodo : todo)));
-            setEditedTodoId(null);
-            setEditedTodoText("");
-        }
-    };
-
-    //Function to delete todo
-    const handleDeleteTodo = (id:number) => {
-        TodoService.deleteTodo(id);
-        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-    }
+  //Function to delete todo
+  const handleDeleteTodo = (id: number) => {
+    TodoService.deleteTodo(id);
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
 
   return (
-    <div className=''>
-      
+    <div className="">
+      <div className=""></div>
+      {todos.map((todo) => (
+        <div key={todo.id} className="">
+          {editedTodoId === todo.id ? (
+            <div className="editedText">
+              <input
+                type="text"
+                value={editedTodoText}
+                onChange={(e) => setEditedTodoText(e.target.value)}
+                autoFocus={true}
+              />
+              <button onClick={() => handleEditSave(todo.id)}>Save</button>
+              <button onClick={handleEditCancel}>Cancel</button>
+            </div>
+          ) : (
+            <div>
+              <span>{todo.text}</span>
+              <button onClick={() => handleEditStart(todo.id, todo.text)}>
+                Edit
+              </button>
+              <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
